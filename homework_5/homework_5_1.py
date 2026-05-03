b3d19e5f901f5fb0379c531c301ab3c740c054e0
YaroshAnton718 <antonyarosh139@gmail.com>
2026-05-03 16:35:51 +0300

    partially completed homework 5_1

--- homework_5/homework_5_1.py
+++ homework_5/homework_5_1.py
@@ -1,2 +1,21 @@
+import string
+import keyword
+
+flag = True
+string_punctuation_without_underline = string.punctuation.replace("_", "")
+
 user_input_string = input("Enter a string: ")
 
+for element in user_input_string:
+    if element in string_punctuation_without_underline or element == " ":
+        flag = False
+
+if user_input_string[0].isdigit() or not user_input_string.islower():
+    flag = False
+
+user_input_words = user_input_string.split()
+for word in user_input_words:
+    if word in keyword.kwlist:
+        flag = False
+
+print(f"{user_input_string} => {flag}")
